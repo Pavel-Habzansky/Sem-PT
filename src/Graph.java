97e438ee8a872272089a943d24d9586066e4b51a
Pavel Habzansky <habzansky.pavel@gmail.com>
2017-11-17 23:47:13 +0100

    Code cleanup

--- src/Graph.java
+++ src/Graph.java
@@ -103,35 +103,6 @@ public class Graph {
         return false;
     }
 
-//    public void dfs(Node start, Node end, ArrayList<Node> visited) {
-//        Node current;
-//        visited.add(start);
-//        for (int i = 0; i < matrix[0].length; i++) {
-//            if (matrix[indexMap.get(start.getId())][i] != null) {
-//                current = new Node((String) indexMap.keySet().toArray()[i]);
-//                if (indexMap.get(start.getId()).equals(indexMap.get(end.getId()))) {
-//                    printPath(visited);
-//                    if (matrix[indexMap.get(visited.get(0).getId())]
-//                            [indexMap.get(visited.get(1).getId())] != null) {
-//                        double totalBandwidth = matrix[indexMap.get(visited.get(0).getId())]
-//                                [indexMap.get(visited.get(1).getId())].getBandwidth();
-//                        for (int j = 0; j < visited.size() - 1; j++) {
-//                            totalBandwidth += matrix[indexMap.get(visited.get(j).getId())]
-//                                    [indexMap.get(visited.get(j + 1).getId())].getBandwidth();
-//                        }
-//                        System.out.println("Total bandwidth of path: " + totalBandwidth);
-//                        visited.remove(start);
-//                        return;
-//                    }
-//                }
-//                if (!(visited.contains(current))) {
-//                    dfs(current, end, visited);
-//                }
-//            }
-//        }
-//        visited.remove(start);
-//    }
-
     // TODO just a test
     public void sendDataPackets(IPacket data) {
         Node nextJump = data.getPath().pop();
