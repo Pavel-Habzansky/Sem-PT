762f914c28b81019b72bb2a9134dd0bab93b2d1c
mikesjcz <32790054+mikesjcz@users.noreply.github.com>
2017-11-20 18:11:01 +0100

    Add files via upload

--- src/Data.java
+++ src/Data.java
@@ -2,7 +2,9 @@
 import java.util.Stack;
 
 /**
- * Created by Jakub Mikeš on 14.11.17.
+ * Data class
+ * @author Habyansky, Mikes
+ *
  */
 public class Data implements IPacket {
 
@@ -12,6 +14,12 @@ public class Data implements IPacket {
     private Node destination;
     private Path path;
 
+    /**
+     * constructor of Data class
+     * @param size
+     * @param source
+     * @param destination
+     */
     public Data(double size, Node source, Node destination) {
         this.size = size;
         this.source = source;
@@ -20,38 +28,71 @@ public class Data implements IPacket {
     }
 
 
-
+/**
+ * the method setPath set new path
+ * @param newPath
+ */
     public void setPath(Path newPath) {
         this.path = newPath;
     }
 
+    /**
+     * the method setSize set new size
+     * @param newSize
+     */
     public void setSize(double newSize) {
         this.size = newSize;
     }
 
+    /**
+     * the method setPosition set new position of node
+     */
     @Override
     public void setPosition(Node newPosition) {
         this.position = newPosition;
     }
 
+    /**
+     * the method getPath return path
+     * @return path
+     */
     @Override
     public Path getPath() {
         return path;
     }
 
+    /**
+     * the method getSize return size
+     * @return size
+     */
     @Override
     public double getSize() {
         return size;
     }
 
+    /**
+     * the method getSource return source
+     * @return source
+     */
     @Override
     public Node getSource() {
         return source;
     }
+    
+    /**
+     * the method getDestination return destionation of node
+     * @return restination
+     *     
+     */
     @Override
     public Node getDestination() {
         return destination;
     }
+    
+    /**
+     * the method getPosition return position of node
+     * @return position
+     */
     @Override
     public Node getPosition() {
         return position;
